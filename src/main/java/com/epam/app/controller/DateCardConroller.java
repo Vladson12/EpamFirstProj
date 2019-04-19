7bbf1a8df81fc9982e673f2d9d9de0e305997326
kseniaser <kseniaser97@gmail.com>
2019-04-20 00:06:26 +0300

    fix adding cards of user

--- src/main/java/com/epam/app/controller/DateCardConroller.java
+++ src/main/java/com/epam/app/controller/DateCardConroller.java
@@ -18,12 +18,19 @@ public class DateCardConroller extends HttpServlet {
         String date = req.getParameter("date");
         String id = req.getParameter("id");
         String login = req.getParameter("login");
-        if (date != null ) {
-            new CardService().updateCardStatusAndDate(new CardService().get(Integer.parseInt(id)),
-                    CardState.AT_HALL, LocalDate.parse(date).plusDays(1));
+        if (!date.isEmpty()) {
             req.setAttribute("login", login);
+            req.setAttribute("id", id);
+            LocalDate newDate = LocalDate.parse(date).plusDays(1);
+            if (LocalDate.now(ZoneId.of("Asia/Tokyo")).compareTo(newDate)>0){
+                req.getRequestDispatcher("view/dataFormat.jsp").forward(req, resp);
+            }
+            new CardService().updateCardStatusAndDate(new CardService().get(Integer.parseInt(id)),
+                    CardState.AT_HALL, newDate);
             req.getRequestDispatcher("/cards").forward(req, resp);
         }
+        req.setAttribute("login", login);
+        req.setAttribute("id", id);
         req.getRequestDispatcher("view/dataFormat.jsp").forward(req, resp);
 
 
