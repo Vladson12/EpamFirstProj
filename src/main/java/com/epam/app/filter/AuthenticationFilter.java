51a62198f0dffd54dfa6a3ccd05c473cfdddd04c
Konstantin <kos.krylov@mail.ru>
2019-05-07 18:06:12 +0300

    finish filter security

--- src/main/java/com/epam/app/filter/AuthenticationFilter.java
+++ src/main/java/com/epam/app/filter/AuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.epam.app.filter;
 
-import com.epam.app.model.User;
 import org.apache.log4j.Logger;
 
 import javax.servlet.*;
@@ -9,44 +8,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebFilter("/AuthenticationFilter")
 public class AuthenticationFilter implements Filter {
 
     static final Logger log = Logger.getLogger(AuthenticationFilter.class);
-
-    private String pathToBeIgnored;
-    private String pathToBeIgnored2;
-    private String pathToBeIgnored3;
-    private String pathToBeIgnored4;
+    private final static Map<String, String> initValues = new HashMap<>();
 
     public void init(FilterConfig fConfig) throws ServletException {
-        pathToBeIgnored = fConfig.getInitParameter("pathToBeIgnored");
-        pathToBeIgnored2 = fConfig.getInitParameter("pathToBeIgnored2");
-        pathToBeIgnored3 = fConfig.getInitParameter("pathToBeIgnored3");
-        pathToBeIgnored4 = fConfig.getInitParameter("pathToBeIgnored4");
         log.info("AuthenticationFilter initialized");
+
+        Enumeration<String> initParameterNames = fConfig.getInitParameterNames();
+        while (initParameterNames.hasMoreElements()) {
+            String paramName = initParameterNames.nextElement();
+            String paramValue = fConfig.getInitParameter(paramName);
+            initValues.put(paramName, paramValue);
+        }
     }
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-
         String uri = req.getRequestURI();
+        HttpSession session = req.getSession();
 
-        HttpSession session = req.getSession(false);
-        User user = (User) session.getAttribute("loggedInUser");
-
-        if ((session == null || user == null) &&
-                !(uri.equals(pathToBeIgnored) || (uri.equals(pathToBeIgnored2))) &&
-                !(uri.equals(pathToBeIgnored3) || (uri.equals(pathToBeIgnored4)))) {
+        if ((session == null || session.getAttribute("loggedInUser") == null) &&
+                !(initValues.values().stream().anyMatch(s -> s.equals(uri)))) {
             res.sendRedirect(req.getContextPath() + "/login");
-            log.info("Unauthorized access request");
         } else {
             chain.doFilter(request, response);
         }
     }
+
 
 
     public void destroy() {
